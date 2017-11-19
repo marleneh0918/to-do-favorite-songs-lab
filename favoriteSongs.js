28d5c9c3fc6fc8a7eac6346ea8b2f35d2eedf740
Marlene Hernandez <marleneh50324@students.laalliance.org>
2017-11-19 06:19:50 +0000

    first commit

---
+++ favoriteSongs.js
@@ -0,0 +1,10 @@
+var favoriteSongs =[
+    "The Party & After Party",
+	"Why'd You Only Call Me When You're High",
+	"Havana",
+	"Kill Kill",
+	"Mermaid Hotel"
+    ]
+    for (var i=0; i<favoriteSongs.length; i++){
+        console.log('#' + (i+1) + ':' + favoriteSongs[i])
+    }
